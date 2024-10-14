1dc4d74b09e1a1995703b5476c6a3e64ebba4597
Andres Muniz Piniella <amp-git@c4ad.eu>
2024-10-14 11:58:00 +0100

    Giving up on ts but seems I was close.

--- src/app/page_old.tsx
+++ src/app/page_old.tsx
@@ -1,6 +1,15 @@
 import Image from 'next/image'
+// import { useEffect } from 'react'
 
 export default function Home() {
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const response = await fetch('/api/scrape')
+  //     const data = await response.json()
+  //     console.log(data)
+  //   }
+  //   fetchData()
+  // }, [])
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
       <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
--- src/utils/scrapper.ts
+++ src/utils/scrapper.ts
@@ -1,5 +1,4 @@
 const puppeteer = require('puppeteer')
-
 const fs = require('fs')
 
 const componentArray = []
@@ -13,7 +12,25 @@ export default async function scrapper(url: string) {
     })
     const page = await browser.newPage()
     await page.goto(url)
+    sweepPage(page, browser)
   } catch (error) {
     console.error(error)
   }
+}
+const sweepPage = async (page, browser) => {
+  //::Element[] no me funciona?
+  console.log('Starting sweep of page')
+
+  const Navlist = await page.$$(
+    '#navlist > li > div[class="s-productthumbbox"]'
+  )
+  // console.log(Navlist)
+  const img = await page.$$eval(
+    '#navlist > li > div[class="s-productthumbbox"] ',
+    (elements) => {
+      console.log('These are the elements', elements)
+      return elements //.map((e) => e.src)
+    }
+  )
+  console.log('The images are:', img)
 }
